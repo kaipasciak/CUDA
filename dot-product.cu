8d028e3a5bf3378a94e5b94ea69ad65f2432ae37
Kai Pasciak <kai.pasciak@gmail.com>
2024-04-29 13:56:17 -0400

    Started

---
+++ dot-product.cu
@@ -0,0 +1,20 @@
+// CUDA Dot Product Program
+// CS 3220
+// Authors: Kai Pasciak, Walter Clay
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 65536 // 256 * 256
+
+__global__
+void dotp( float *u, float *v, float *partialSums, int n ){
+
+
+}
+
+int main(){
+
+
+    return 0;
+}
---
+++ matrix-vector-product.cu
@@ -0,0 +1,20 @@
+// CUDA Matrix Vector Product Program
+// CS 3220
+// Authors: Kai Pasciak, Walter Clay
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5000
+
+__global__
+void MxV( float *M, float *x, float *y, int n){
+
+
+}
+
+int main(){
+
+
+    return 0;
+}
