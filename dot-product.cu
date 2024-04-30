1e8f0093758e3bcedf54b19923ef03b41e3622b4
Kai Pasciak <kai.pasciak@gmail.com>
2024-04-29 22:01:01 -0400

    Finished most of code in dot p

--- dot-product.cu
+++ dot-product.cu
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
 
 #define N 65536 // 256 * 256
 
@@ -34,27 +36,82 @@ void dotp( float *u, float *v, float *partialSums, int n ){
 }
 
 int main(){
+    // Initialize and define constants
+    int numBlocks = 256;
+    int threadsPerBlock = 256;
+
+    // Initialize variables
+    float *U, *V, *partialSum;
+    float dev_U, dev_V;
+
+    // Allocate memory on the host
     U = (float *) malloc(N * sizeof(float));
     V = (float *) malloc(N * sizeof(float));
     partialSum = (float *) malloc(numBlocks * sizeof(float));
+
+    // Allocate memory on the device
+    cudaMalloc( (void **) &dev_U, N*sizeof(float) );
+    cudaMalloc( (void **) &dev_V, N*sizeof(float) );
+
+
+    // Set up problem on the host
+    // Create seed for random number generator
+    srand48(time(nullptr));
+
+    // Set vector contents to random numbers
     for (int i = 0; i < N; ++i) {
-        U[i] = (float) (i + 1);
-        V[i] = 1.0 / U[i];
+        float randomU == drand48();
+        U[i] = random U;
+
+        float randomV == drand48();
+        V[i] = random V;
     }
 
+    // Copy data to the GPU
     cudaMemcpy( dev_U, U, N*sizeof(float), cudaMemcpyHostToDevice );
     cudaMemcpy( dev_V, V, N*sizeof(float), cudaMemcpyHostToDevice );
 
     // Start timer
-    // TODO: Implement events for elapsed time
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start,0);
+
+    // Call kernel
     dotp<<<numBlocks, threadsPerBlock>>>( dev_U, dev_V, dev_partialSum, N );
+
+    // Synchronize
     cudaDeviceSynchronize();
-    cudaMemcpy( partialSum, dev_partialSum, numBlocks*sizeof(float), cudamemcpyDeviceToHost);
+
+    // End timer
+    cudaEventRecord(stop, 0);
+
+    // Copy results to host
+    cudaMemcpy( partialSum, dev_partialSum, numBlocks*sizeof(float), cudaMemCpyDeviceToHost);
+
+    // Calculate elapsed time
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, stop);
 
     // Sum partial sums
     float gpuResult = 0.0;
     for (int i = 0; i < numBlocks; ++i)
         gpuResult = gpuResult + partialSum[i];
+
+    // TODO: Implement CPU calculation and time
+
+    // TODO: Calculate relative error
+
+    // Clean up
+    cudaFree( dev_U );
+    cudaFree( dev_V );
+
+    free(U);
+    free(V);
+    free(partialSum);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
 
     return 0;
 }
